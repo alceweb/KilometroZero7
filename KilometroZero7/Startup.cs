d476fcfc65ac537d7fb71ae49f60aef21452a2ea
cesare rocchetti <cesare@cr-consult.eu>
2015-09-17 08:18:26 +0200

    primo commit ruoli ok

---
+++ KilometroZero7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KilometroZero7.Startup))]
+namespace KilometroZero7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
