08c90af31b6ac52f68c1ebe39a99403c29dfb4ef
cesare rocchetti <cesare@cr-consult.eu>
2015-10-02 07:27:48 +0200

    Immagini su Jumbotron

--- KilometroZero7/Controllers/HomeController.cs
+++ KilometroZero7/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using KilometroZero7.Models;
 
 namespace KilometroZero7.Controllers
 {
@@ -38,5 +41,31 @@ namespace KilometroZero7.Controllers
         {
             return View();
         }
+
+        public async Task<ActionResult> MailCreditComune(MailCreditComune model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<h1>Kilometro Zero</h1><h2>Richiesta attivazione comune</h2><p><strong>Ricevuta da:</strong> {0} ({1})</p><p><strong>Indirizzo:</strong> {2}</p><p>{3} - {4} ({5})</p><p><strong>Telefono:</strong> {6}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("cesare@cr-consult.eu"));  // replace with valid value 
+                message.Subject = "Richiesta attivazione comune";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.FromIndirizzo, model.FromCap, model.FromComune, model.FromPro, model.FromTel);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+
+        }
+        public ActionResult SentCreditComune()
+        {
+            return View();
+        }
     }
+
 }
---
+++ KilometroZero7/Models/MailCreditComune.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KilometroZero7.Models
+{
+    public class MailCreditComune
+    {
+        [Required, Display(Name = "Nome Cognome")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required, Display(Name = "Telefono")]
+        public string FromTel { get; set; }
+        [Required, Display(Name = "Provincia")]
+        public string FromPro { get; set; }
+        [Required, Display(Name = "Indirizzo municipio")]
+        public string FromIndirizzo { get; set; }
+        [Required, Display(Name = "CAP")]
+        public string FromCap { get; set; }
+        [Required, Display(Name = "Comune")]
+        public string FromComune { get; set; }
+
+    }
+}
